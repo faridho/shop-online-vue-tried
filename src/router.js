e3a83c8b431e74dbbc9e2aad8348fec113b8ba5e
faridho <faridho45@gmail.com>
2019-09-23 19:23:12 +0700

    first commit

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Frame from './views/Frame'
 
 Vue.use(Router)
 
@@ -9,17 +9,10 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/frame',
+      name: 'frame',
+      component: Frame
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    
   ]
 })
---
+++ src/views/Frame/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+<script>
+  export default {
+    data: vm => ({
+      title: 'this is a frame',
+      activeIndex: '1',
+
+      slides: [
+
+      ]
+    }),
+
+    methods: {
+
+    }
+  }
+</script>
