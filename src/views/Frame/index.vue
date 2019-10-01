b6cc2f56a4db3d2df6208953bc2c9181042d5161
faridho <faridho45@gmail.com>
2019-10-01 23:25:57 +0700

    navbar done

--- src/views/Frame/index.vue
+++ src/views/Frame/index.vue
@@ -1,21 +1,46 @@
 <template>
   <div>
-    {{ title }}
+    <div class="responsive-menu">
+      <a href="javascript:void(0);" @click="toogleMenu">Menu</a>
+    </div>
+    <div class="top-nav" v-bind:class="[responsiveActive ? '': 'responsive-navbar']">
+      <a href="" class="active">Home</a>
+      <div class="drop-down">
+        <a href="">Dress</a>
+        <div class="drop-down-content">
+          <a href="">T-Shirts</a>
+          <a href="">Pants</a>
+          <a href="">Jeans</a>
+        </div>
+      </div>
+      <div class="drop-down">
+        <a href="">Accesories</a>
+        <div class="drop-down-content">
+          <a href="">Watch</a>
+          <a href="">Wallet</a>
+        </div>
+      </div>
+      <div class="search-section">
+        <input type="text" class="search-navbar" placeholder="Search..."/>
+        <button class="btn-search-navbar"> Search</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
   export default {
-    data: vm => ({
-      title: 'this is a frame',
-      activeIndex: '1',
-
-      slides: [
-
-      ]
+    data: () => ({
+     responsiveActive: true
     }),
 
     methods: {
-
+      toogleMenu() {
+        if(this.responsiveActive)  {
+          this.responsiveActive = false;
+        } else {
+          this.responsiveActive = true;
+        }
+      }
     }
   }
 </script>
